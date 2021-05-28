cb364b2dc59d84fda5b572ca3dc18b76c8cda295
Christopher Stevers <72156679+Christopher-Stevers@users.noreply.github.com>
2021-05-28 14:14:15 -0400

    initial commit

---
+++ Migrations/20210526100659_otherCreate.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace finalApp.Migrations
+{
+    public partial class otherCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
